eb2d6c4e179f718940a09fe5bec8617966c9aee3
Suruchi Jha <suruchiofficial9@gmail.com>
2026-06-05 19:40:34 +0530

    Add files via upload

---
+++ prooject_intern/script.js
@@ -0,0 +1,32 @@
+
+// Welcome Message
+alert("Welcome to Pastel Dreams 🎧💖");
+
+// Play Music
+function playMusic(){
+
+    alert("Playing Music 🎵");
+
+}
+
+// Premium Button
+function premium(){
+
+    alert("Premium Feature Coming Soon ✨");
+
+}
+
+// Change Background Theme
+function changeTheme(){
+
+    document.body.style.background =
+    "linear-gradient(to right,#ffcce6,#d9b3ff)";
+
+}
+
+// Thank You Message
+function thankYou(){
+
+    alert("Thanks for visiting Pastel Dreams 💜");
+
+}
